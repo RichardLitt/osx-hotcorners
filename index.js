8bff8ca35482bbe87aa7e2e9bbe674d235810080
Richard Littauer <richard.littauer@gmail.com>
2016-12-25 17:12:15 -0500

    Loop over error to fix it

--- index.js
+++ index.js
@@ -3,9 +3,13 @@ const applescript = require('applescript')
 const path = require('path')
 
 // TODO Manually set hotcorners using args
+function test () {
+  applescript.execFile(path.join(__dirname, './hotcorners.scpt'), function (err, rtn) {
+    if (err) {
+      // TODO Fix error with error needing to be run again.
+      test()
+    }
+  })
+}
 
-applescript.execFile(path.join(__dirname, './hotcorners.scpt'), function (err, rtn) {
-  if (err) {
-    console.log('Error', err) // Something went wrong!
-  }
-})
+test()
